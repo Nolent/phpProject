f120c56bc03dc0b472e270aec4199ac911dd5e8f
salutcestkaki <kaki_98@hotmail.fr>
2017-10-02 15:31:35 +0200

    index change

--- 9_5.php
+++ 9_5.php
@@ -4,76 +4,76 @@
 
 <?php
 $erreur = true;
-if ( !empty($_POST ))	
-//if ( isset ($_POST["nom"]) && isset($_POST["prenom"]) )	
+if ( !empty($_POST ))
+//if ( isset ($_POST["nom"]) && isset($_POST["prenom"]) )
 {
 	echo"<pre>";
 	print_r($_POST);
 	echo "</PRE>";
 	echo "décodage sur le serveur <br />";
-	  
+
 	$erreur = false;
-	if (!empty($_POST['nom']) && $_POST['nom'] != "nom ?") 
-    $nom = $_POST['nom'];  
-	else 
+	if (!empty($_POST['nom']) && $_POST['nom'] != "nom ?")
+    $nom = $_POST['nom'];
+	else
 	{
 		echo "le nom est vide <br />";
 		$erreur = true;
 	}
-	if (!empty($_POST['prenom']) && $_POST['prenom'] != "") 
-		$prenom = $_POST['prenom'] ; 
-	else 
+	if (!empty($_POST['prenom']) && $_POST['prenom'] != "")
+		$prenom = $_POST['prenom'] ;
+	else
 	{
 		echo "le prénom est vide <br />";
 		$erreur = true;
 	}
-	if (!empty($_POST['courriel']) && $_POST['courriel'] != "courriel ?") 
-		$courriel = $_POST['courriel'] ; 
-	else 
+	if (!empty($_POST['courriel']) && $_POST['courriel'] != "courriel ?")
+		$courriel = $_POST['courriel'] ;
+	else
 	{
 		echo "le courriel est vide <br />";
 		$erreur = true;
 	}
-	if (!empty($_POST['gouts']) && $_POST['gouts'] != "quoi d'autre ? ?") 
-		$gouts = $_POST['gouts'] ; 
+	if (!empty($_POST['gouts']) && $_POST['gouts'] != "quoi d'autre ? ?")
+		$gouts = $_POST['gouts'] ;
 	else 
 	{
 		echo "les autres gouts ne sont pas remplis <br />";
 		$erreur = true;
 	}
-	if (!empty($_POST['code'])) 
-		$code = $_POST['code'] ; 
-	else 
+	if (!empty($_POST['code']))
+		$code = $_POST['code'] ;
+	else
 	{
 		echo "le mot de passe est vide <br />";
 		$erreur = true;
 	}
-	if (isset($_POST['civilite']) ) 
-		$civ = $_POST['civilite'];  
-	else 
+	if (isset($_POST['civilite']) )
+		$civ = $_POST['civilite'];
+	else
 	{
 		echo "la civilité n'a pas été cochée <br />";
 		$erreur = true;
 	}
-	if (isset($_POST['pays']) ) 
-		$pays = $_POST['pays'];  
-	else 
+	if (isset($_POST['pays']) )
+		$pays = $_POST['pays'];
+	else
 	{
 		echo "le pays n'a pas été sélectionné <br />";
 		$erreur = true;
 	}
-	if (isset($_POST['preference']) ) 
-		$pref = $_POST['preference'];  
-	else 
+	if (isset($_POST['preference']) )
+		$pref = $_POST['preference'];
+	else
 	{
 		echo "aucune case n'a pas été cochée <br />";
 		$erreur = true;
 	}
-	
-	
-	if (!empty($_POST['discret']) ) 	
+
+
+	if (!empty($_POST['discret']) )
 		$val = $_POST['discret'];
-			
+
 	if ($erreur == false)
 	{
 		echo "NOM : $nom <br />";
@@ -93,11 +93,11 @@ if ( !empty($_POST ))
 		$fic4 = $_FILES['fichier']['tmp_name'];
 		$fic5 = $_FILES['fichier']['error'];
 
-		echo "fic1 : $fic1<br>"; 
-		echo "fic2 : $fic2<br>"; 
-		echo "fic3 : $fic3<br>"; 
-		echo "fic4 : $fic4<br>"; 
-		echo "fic5A : $fic5<br>"; 
+		echo "fic1 : $fic1<br>";
+		echo "fic2 : $fic2<br>";
+		echo "fic3 : $fic3<br>";
+		echo "fic4 : $fic4<br>";
+		echo "fic5A : $fic5<br>";
 
 		$result=move_uploaded_file($fic4,$fic1);
 		if($result==TRUE)
