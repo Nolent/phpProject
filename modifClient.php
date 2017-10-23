35eb27d607095302013c7b77e8fb96c8c3ed6b49
salutcestkaki <kaki_98@hotmail.fr>
2017-10-23 15:15:41 +0200

    modif requete updateClient 	modifié :         modifClient.php

--- modifClient.php
+++ modifClient.php
@@ -1,17 +1,11 @@
-<!--  E.Porcq	e95.php 20/09/2010 -->
-
-<!-- maison : http://localhost:8082/support_php/prof/e95.php -->
 
 <?php
-include ('accesTable.php');
-$conn = connect();
-
 
 function remplirOption($tab, $nbLignes)
 {
     for ($i=0; $i<$nbLignes; $i++) {
-        $tab[$i]["NOM"] = utf8_encode($tab[$i]["NOM"]);
-        if ($tab[$i]['NOM']=="FRANCE") {
+        $tab[$i]["CL_NOM"] = utf8_encode($tab[$i]["CL_NOM"]);
+        if ($tab[$i]['CL_NOM']=="FRANCE") {
             echo '<option value='.$tab[$i]['NOM'].' selected >'.$tab[$i]['NOM'];
         } else {
             echo '<option value='.$tab[$i]['NOM'].'>'.$tab[$i]['NOM'];
@@ -19,103 +13,25 @@ function remplirOption($tab, $nbLignes)
         echo '</option>';
     }
 }
-function ListePays()
+
+function ListeClient()
 {
-    //$login = 'ETU2_53';
-    //$mdp = 'ETU2_53';
-    //$instance = 'spartacus.iutc3.unicaen.fr:1521/info.iutc3.unicaen.fr';
+    $login = 'ETU2_53';
+    $mdp = 'ETU2_53';
+    $instance = 'spartacus.iutc3.unicaen.fr:1521/info.iutc3.unicaen.fr';
     // ce code ne doit pas être dans le <select> … </select>
     global $conn;
-    $req = 'SELECT * FROM CDI_PAYS order by nom';
+    $req = 'SELECT * FROM CDI_CLIENT order by cl_nom';
     $cur = PreparerRequete($conn, $req);
     $res = ExecuterRequete($cur);
     $nbLignes = LireDonnees1($cur, $tab); // Attention, pas &$tab
     if (!empty($_POST)) {
-        if (isset($_POST['pays'])) {
-            $cour = $_POST['pays'];
-            echo ("Pays $cour sélectionné");
+        if (isset($_POST[''])) {
+            $cour = $_POST[''];
+            echo ("Client $cour sélectionné");
         }
     }
     remplirOption($tab, $nbLignes);
 }
-$erreur = true;
-if (!empty($_POST )) {
-//if ( isset ($_POST["nom"]) && isset($_POST["prenom"]) )
-    $erreur = false;
-    if (!empty($_POST['nom']) && $_POST['nom'] != "nom ?") {
-        $nom = $_POST['nom'];
-    } else {
-        $erreur = true;
-    }
-    if (!empty($_POST['prenom']) && $_POST['prenom'] != "prenom") {
-        $prenom = $_POST['prenom'] ;
-    } else {
-        $erreur = true;
-    }
-    if (!empty($_POST['localite']) && $_POST['localite'] != "ville") {
-        $localite = $_POST['localite'] ;
-    } else {
-        $erreur = true;
-    }
-    if (isset($_POST['type'])) {
-        $type = $_POST['type'];
-    } else {
-        $erreur = true;
-    }
-    if (isset($_POST['pays'])) {
-        $pays = $_POST['pays'];
-    } else {
-        $erreur = true;
-    }
-    if ((!isset($_POST["CA"]) || empty($_POST["CA"])) || $_POST["type"] == "Particulier") {
-        $ca = null;
-    } elseif (!empty($_POST['CA'])) {
-        $ca = $_POST["CA"];
-    } else {
-        $erreur = true;
-    }
 
-    if ($erreur == false) {
-        include ('verifNom.php');
-        $tab = verifAndConvertAll($nom, $prenom, $localite);
-
-        $nom = verifAndConvert($nom);
-        $prenom = verifAndConvert($prenom, true);
-        $localite = verifAndConvertVille($localite);
-
-
-        if ($nom != 1 && $prenom != 1 && $localite != 1 && verifChiffre($ca) == 0) {
-            $req = "select cl_numero from cdi_client where cl_nom = '$nom' and cl_prenom='$prenom' and cl_localite = '$localite'";
-            $cur = PreparerRequete($conn, $req);
-            ExecuterRequete($cur);
-            $nbLignes;
-            $tab;
-            $nbLignes = LireDonnees2($cur, $tab);
-            if ($nbLignes == 0) {
-                updateClient($nom, $prenom, $pays, $localite, $type, $ca);
-                include ("modifClient.htm");
-                echo '<script>alert(Entrée dans la base réussie)</script>';
-            } else {
-                include ("modifClient.htm");
-                echo '<script>alert("Client déjà présent")</script>';
-            }
-        } else {
-            $erreur = true;
-            if ($tab['nom'] == 1) {
-                echo '<script>alert("Charactere interdit dans nom")</script>';
-            } elseif ($tab['prenom'] == 1) {
-                echo '<script>alert("Charactere interdit dans prenom")</script>';
-            } elseif ($localite == 1) {
-                echo '<script>alert("Charactere interdit dans ville")</script>';
-            } else {
-                echo '<script>alert("Il ne peut y avoir que des nombres dans CA")</script>';
-            }
-        }
-    }
-}
-if ($erreur == true) {
-    include ("utils.php");
-    include ("modifClient.htm");
-} else {
-}
 ?>
